0e7b7ec219f709c014ad07246025a51726027e6a
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-07-17 09:15:19 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -55,19 +55,4 @@ export class App extends Component {
     );
   }
 }
-// export class App extends Component = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
+
